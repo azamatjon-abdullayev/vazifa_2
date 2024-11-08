8742129c57d648d705d47131405a7310f2ff1177
Azamatjon <146099098+azamatjon-abdullayev@users.noreply.github.com>
2024-11-08 10:30:11 +0300

    first commit

---
+++ vazifa_2.sql
@@ -0,0 +1,11 @@
+DELETE FROM projects WHERE pr_id = 2;
+
+
+update projects set title = 'New Project Title' where pr_id = 1 ;
+
+
+
+select * from departments;
+select * from Employees;
+select * from Projects;
+select * from tasks;
